0d2636a2c3af2020efb424860a985ccfd48a56bc
Fabian Steeg <fsteeg@gmail.com>
2007-07-11 22:00:12 +0000

    FDG-Paper complete

---
+++ de.uni_koeln.spinfo.fg/fdg/paper/fdg-paper/images.pl
@@ -0,0 +1,65 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/{figure}center{preform{<verbatim_mark>verbatim151#preform{{{center{{figure};FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="632" HEIGHT="223" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="\begin{figure}\begin{center}
+\begin{verbatim}verb(
+give,
+action,
+[gave, g...
+...],
+[recipient, animate, X3]
+],
+Sat
+).\end{verbatim}
+\end{center}\end{figure}">|; 
+
+$key = q/fbox{includegraphics[width=15cm]{screen2.png}};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="695" HEIGHT="1114" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\fbox{\includegraphics[width=15cm]{screen2.png}}">|; 
+
+$key = q/includegraphics[width=15cm]{..slashtree-part.pdf};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="241" HEIGHT="225" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="\includegraphics[width=15cm]{../tree-part.pdf}">|; 
+
+$key = q/{figure}center{preform{<verbatim_mark>verbatim148#preform{{{center{{figure};FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="695" HEIGHT="1114" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="\begin{figure}\begin{center}
+\begin{verbatim}(Past e:
+(d1x:'man'[N]:
+(Past P...
+...man'[N])RecSubj
+)
+)
+(d1x:'john'[N])0
+)\end{verbatim}
+\end{center}\end{figure}">|; 
+
+$key = q/{figure}center{preform{<verbatim_mark>verbatim149#preform{{{center{{figure};FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="315" HEIGHT="184" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="\begin{figure}\begin{center}
+\begin{verbatim}(p1:[
+(Past e1:[
+(f1:tek[
+(x1...
+...Ag
+(x3:mi(x3))Pat
+](f2))
+](e1))
+](p1))\end{verbatim}
+\end{center}\end{figure}">|; 
+
+1;
+
---
+++ de.uni_koeln.spinfo.fg/fdg/paper/fdg-paper/internals.pl
@@ -0,0 +1,78 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/uml-tree/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_HengeveldAndMackenzie2006/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/parser-overview/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fdg-rl/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_HuntAndThomas1999/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/web/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/prolog-2/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/antlr-input/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Dik1992/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Samuelsdorff1989/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/antlr-tree/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/antlr-rl-parser/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Bakker1994/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/console/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Dik1997a/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sysflow/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Parr2007/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/dsls/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
